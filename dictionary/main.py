b74b9e5ea38f713a06a9b593fae6dac49c5e7346
Katerina <katerina.zuzanakova@gmail.com>
2018-10-29 16:48:43 +0100

    Flake8 check (main.py)

--- dictionary/main.py
+++ dictionary/main.py
@@ -1,24 +1,24 @@
 #############################################################################
-#                                                      
-# Czech Sign Language Dictionary 
+#
+# Czech Sign Language Dictionary
 #
 #
 # A GUI application that serves as a bilingual dictionary for translating
 # expressions in both directions: to and from czech and czech sign language.
-#    
-# A czech expression may be entered by typing into an entry or by 
+#
+# A czech expression may be entered by typing into an entry or by
 # selecting from a chosen category (or subcategory) of words.
 # If a sign language translation of the expression is found in the database,
-# a video with the sign is played. If there's more than one possible 
+# a video with the sign is played. If there's more than one possible
 # translation, all the possibilities are displayed in thumbnail videos.
-#    
+#
 # An expression in sign language is entered by specifying the sign's
 # components (handshapes, placement) and the sign type.
 # The application tries to find signs that correspond the best to the
 # components entered by the user. These signs are then displayed as thumbnail
 # videos. After double clicking on them, the video with corresponding czech
 # translation is shown.
-#                                                      
+#
 #
 # The application is released under the MIT license.
 #
@@ -31,7 +31,7 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in 
+# The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -45,7 +45,7 @@
 #############################################################################
 
 
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
 import os
@@ -57,11 +57,11 @@ from sign_input_frame import SignInputFrm
 
 class Dictionary():
     """The main Czech Sign Language Dictionary application class.
-    
-    Creates the root application window with all its descending widgets, 
-    as well as an object (self.searchEng) that provides the logic behind 
+
+    Creates the root application window with all its descending widgets,
+    as well as an object (self.searchEng) that provides the logic behind
     the application.
-    
+
     The root application window (self.root) contains:
     - the main frame (self.mainfrm) where the result of search is displayed,
         and where there's also an entry for a czech expression input
@@ -78,103 +78,99 @@ class Dictionary():
 
     def __init__(self, dbpath, vfdir, imgdir):
         """Build the application.
-        
+
         Arguments:
             dbpath (str): the database file path
-            vfdir (str): a path to the directory where video files with 
+            vfdir (str): a path to the directory where video files with
                 translations to sign language are located
             imgdir (str): a path to the directory where images are located
         """
         self.dbpath = dbpath
         self.vfdir = vfdir
         self.imgdir = imgdir
-        
+
         self.altsmax = 10  # number of alternatives showed when word not found
         self.canvasSize = (250, 250)  # canvas for specifying sign placement
-        
+
         # the SearchEngine object provides the logic behind the dictionary app
-        self.searchEng = SearchEngine(self.dbpath, 
-                                      self.vfdir, 
-                                      self.altsmax, 
+        self.searchEng = SearchEngine(self.dbpath,
+                                      self.vfdir,
+                                      self.altsmax,
                                       self.canvasSize)
         self.makeWidgets()
-        
+
     def makeWidgets(self):
         """Build the app window with all its widgets and define a style."""
-        
+
         # build the application window
-        self.root = Tk()
+        self.root = tk.Tk()
         self.root.title('Slovník českého znakového jazyka')
         self.root.config(bg=self.BGCOLOR)
-        self.root.minsize(width = self.WIN_MIN_WIDTH, 
-                          height = self.WIN_MIN_HEIGHT)
-        
+        self.root.minsize(width=self.WIN_MIN_WIDTH,
+                          height=self.WIN_MIN_HEIGHT)
+
         self.font = tkFont.Font(family='Nimbus Sans L', size=12)
         self.root.option_add('*Font', self.font)
-        
+
         self.root.columnconfigure(0, weight=2)  # empty column
         self.root.columnconfigure(2, weight=1)  # empty column
         self.root.columnconfigure(3, weight=3)  # notebook column
         self.root.rowconfigure(0, weight=1)
 
         # create the main frame
-        self.mainfrm = MainFrm(self.root, 
-                               self.dbpath, 
-                               self.vfdir, 
-                               self.imgdir, 
-                               self.searchEng.search, 
-                               self.altsmax, 
-                               self.BORDER, 
-                               bg=self.BGCOLOR, 
+        self.mainfrm = MainFrm(self.root,
+                               self.dbpath,
+                               self.vfdir,
+                               self.imgdir,
+                               self.searchEng.search,
+                               self.altsmax,
+                               self.BORDER,
+                               bg=self.BGCOLOR,
                                padx=self.BORDER)
-        self.mainfrm.grid(column=1, row=0, sticky=N+E+S+W)
-        
+        self.mainfrm.grid(column=1, row=0, sticky=tk.N+tk.E+tk.S+tk.W)
+
         # when the searchEng is done searching, it calls a function to show
         # the result - this function is defined in the mainfrm object
         self.searchEng.showResultFcn = self.mainfrm.showResult
-        
+
         # create notebook
         self.notebook = ttk.Notebook(self.root)
-        self.notebook.grid(column=3, row=0, sticky=N+E+S+W)
-        
+        self.notebook.grid(column=3, row=0, sticky=tk.N+tk.E+tk.S+tk.W)
+
         # style settings
         self.style = style = ttk.Style()
         try:
             style.theme_use('clearlooks')
-        except TclError:
+        except tk.TclError:
             style.theme_use('clam')
-        
-        style.configure('Gray.TEntry', foreground='gray')   # for default text
-        style.configure('Black.TEntry', foreground='black') # for user input
-        
+
+        style.configure('Gray.TEntry', foreground='gray')  # for default text
+        style.configure('Black.TEntry', foreground='black')  # for user input
+
         style.configure('TCombobox', background=self.BGCOLOR)
         style.map('TCombobox', background=[('disabled', self.BGCOLOR)])
-        
-        style.configure('Treeview', background=self.BGCOLOR, 
-                                    rowheight=25, 
-                                    padding=(0, 0, 0, 25))
-                                    # the bottom padding is necessary for the
-                                    # treeview bottom border to be visible
+
+        style.configure('Treeview',
+                        background=self.BGCOLOR,
+                        rowheight=25,
+                        padding=(0, 0, 0, 25))
+        # the bottom padding is necessary for the treeview bottom border
+        # to be visible
+
         style.map('Treeview', background=[('selected', 'lightgrey')])
         style.configure('Treeview.Item', padding=(-15, 0, 0, 0))
-        
+
         # ged rid of dashed rectangle around the highlighted item in treeview
         style.layout('Treeview.Item',
-          [('Treeitem.padding', {'sticky': 'nswe', 'children': 
-            [('Treeitem.indicator', {'side': 'left', 'sticky': ''}),
-             ('Treeitem.image', {'side': 'left', 'sticky': ''}),
-            #('Treeitem.focus', {'side': 'left', 'sticky': '', 'children': [
-               ('Treeitem.text', {'side': 'left', 'sticky': ''}),
-            #]})
-            ],
-          })]
-        )
-        
-        style.configure('TNotebook.Tab', padding=(self.TAB_PAD, 0, 
+                     [('Treeitem.padding', {'sticky': 'nswe', 'children':
+                       [('Treeitem.indicator', {'side': 'left', 'sticky': ''}),
+                        ('Treeitem.image', {'side': 'left', 'sticky': ''})]})])
+
+        style.configure('TNotebook.Tab', padding=(self.TAB_PAD, 0,
                                                   self.TAB_PAD, 0))
-        style.map('TNotebook.Tab', padding=[('selected', (self.TAB_PAD, 0, 
+        style.map('TNotebook.Tab', padding=[('selected', (self.TAB_PAD, 0,
                                                           self.TAB_PAD, 0))])
-            
+
         # tabs' font:
         style.configure('.', font=self.font)
         # color visible only at the notebook border:
@@ -183,28 +179,28 @@ class Dictionary():
         style.configure('TNotebook.Tab', background='ghost white')
         # active tab background:
         style.map('TNotebook.Tab', background=[('selected', self.BGCOLOR)])
-        
+
         # create the category-selection frame
-        self.catfrm = CatFrm(self.notebook, 
-                             self.dbpath, 
-                             self.searchEng.search, 
-                             bg=self.BGCOLOR, 
-                             padx=self.BORDER, 
+        self.catfrm = CatFrm(self.notebook,
+                             self.dbpath,
+                             self.searchEng.search,
+                             bg=self.BGCOLOR,
+                             padx=self.BORDER,
                              pady=self.BORDER)
-        self.catfrm.grid(column=0, row=0, sticky=N+E+S+W)
+        self.catfrm.grid(column=0, row=0, sticky=tk.N+tk.E+tk.S+tk.W)
         self.notebook.add(self.catfrm, text='Výběr podle kategorií')
-        
+
         # create the sign-input frame
-        self.signfrm = SignInputFrm(self.notebook,  
-                                    self.imgdir, 
-                                    self.searchEng.signSearch, 
-                                    self.canvasSize, 
-                                    bg=self.BGCOLOR, 
-                                    padx=self.BORDER, 
+        self.signfrm = SignInputFrm(self.notebook,
+                                    self.imgdir,
+                                    self.searchEng.signSearch,
+                                    self.canvasSize,
+                                    bg=self.BGCOLOR,
+                                    padx=self.BORDER,
                                     pady=20)
-        self.signfrm.grid(column=0, row=0, sticky=N+E+S+W)
+        self.signfrm.grid(column=0, row=0, sticky=tk.N+tk.E+tk.S+tk.W)
         self.notebook.add(self.signfrm, text='Překlad z ČZJ do ČJ')
-        
+
     def positionWindow(self):
         """Position the application window in the center of the screen."""
         self.root.update_idletasks()
@@ -213,15 +209,13 @@ class Dictionary():
         xOffset = self.root.winfo_screenwidth()//2 - width//2
         yOffset = self.root.winfo_screenheight()//2 - height//2
         self.root.geometry('+{}+{}'.format(xOffset, yOffset))
-        
-        
+
+
 if __name__ == '__main__':
     dbpath = os.path.abspath('dict.db')
     vfdir = os.path.abspath('videofiles')
     imgdir = os.path.abspath('images')
-    
+
     dictionary = Dictionary(dbpath, vfdir, imgdir)
     dictionary.positionWindow()
     dictionary.root.mainloop()
-    
-        
