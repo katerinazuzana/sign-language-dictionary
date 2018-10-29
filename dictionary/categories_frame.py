a729437b9b2e7c326022b1ded7d00bcdc8ae1a58
Katerina <katerina.zuzanakova@gmail.com>
2018-10-29 17:57:27 +0100

    Flake8 check (categories_frame.py)

--- dictionary/categories_frame.py
+++ dictionary/categories_frame.py
@@ -1,28 +1,28 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import sqlite3
 from scrolledlist_frame import ScrolledList
 import tools
 
 
-class CatFrm(Frame):
-    """A frame where a word for translation might be selected from 
+class CatFrm(tk.Frame):
+    """A frame where a word for translation might be selected from
     a category or a subcategory of words.
-    
+
     Contains widgets:
     'self.catcb' combobox for choosing a category,
     'self.subcatcb' combobox for choosing one of the category's subcategories,
     'self.scrolledlist' displaying the words from the choosen (sub)category.
-        
-    The user selects the word to be translated from the scrolled list, then 
+
+    The user selects the word to be translated from the scrolled list, then
     a function is called to find and display the sign language translation.
     """
-    
+
     def __init__(self, parent, dbpath, searchfcn, **options):
         """Create the frame with the comboboxes and the scrolled list.
-        
+
         Arguments:
-            parent: the parent tkinter widget 
+            parent: the parent tkinter widget
             dbpath (str): the database file path
             searchfcn: function that does the search, takes one (str) argument
         """
@@ -34,7 +34,7 @@ class CatFrm(Frame):
         self.verticalSpace = 40   # space between widgets
         self.topSpace = 10   # additional padding at the top of the frame
         self.makeWidgets()
-       
+
     def makeWidgets(self):
         """Create a combobox for choosing a category, a combobox for choosing
         a subcategory, and a scrolled list for choosing a word.
@@ -42,67 +42,67 @@ class CatFrm(Frame):
         self.columnconfigure(0, weight=1)  # empty column
         self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=2)  # empty column
-        
+
         # empty rows:
         self.rowconfigure(0, weight=1)
         self.rowconfigure(2, weight=1)
         self.rowconfigure(4, weight=1)
         self.rowconfigure(6, weight=3)
-        
+
         # create category combobox
-        self.catvar = StringVar()
+        self.catvar = tk.StringVar()
         self.catvar.set(' -- Zvolte kategorii --')
-        self.catcb = ttk.Combobox(self, 
-                                  textvariable=self.catvar, 
+        self.catcb = ttk.Combobox(self,
+                                  textvariable=self.catvar,
                                   width=self.width,
                                   state='readonly')
-        
+
         # set up the options available in category combobox
         self.catcb['values'] = self.findCats()
-        
+
         self.catcb.bind('<<ComboboxSelected>>', self.catHandler)
-        self.catcb.grid(column=1, row=1, 
-                        sticky=N+E+S+W, 
+        self.catcb.grid(column=1, row=1,
+                        sticky=tk.N+tk.E+tk.S+tk.W,
                         pady=(self.topSpace, self.verticalSpace))
 
         # create subcategory combobox, initially disabled
-        self.subcatvar = StringVar()
+        self.subcatvar = tk.StringVar()
         self.subcatvar.set(' -- Zvolte podkategorii --')
         self.subcatcb = ttk.Combobox(self,
-                             textvariable=self.subcatvar,
-                             state='disabled',
-                             width=self.width)
-        
+                                     textvariable=self.subcatvar,
+                                     state='disabled',
+                                     width=self.width)
+
         self.subcatcb.bind('<<ComboboxSelected>>', self.subcatHandler)
-        self.subcatcb.grid(column=1, row=3, 
-                           sticky=N+E+S+W, 
+        self.subcatcb.grid(column=1, row=3,
+                           sticky=tk.N+tk.E+tk.S+tk.W,
                            pady=(0, self.verticalSpace))
-        
+
         # create empty scrolledlist
-        self.scrolledlist = ScrolledList(self, 
-                                         self.searchfcn, 
+        self.scrolledlist = ScrolledList(self,
+                                         self.searchfcn,
                                          self.height)
-        self.scrolledlist.grid(column=1, row=5, 
-                               sticky=N+E+W, 
+        self.scrolledlist.grid(column=1, row=5,
+                               sticky=tk.N+tk.E+tk.W,
                                pady=(0, self.verticalSpace))
         self.rowconfigure(2, weight=1)
-    
+
     def findCats(self):
         """Look up available categories in the database and return
         a list of options for the category combobox.
         """
         SQLquery = 'SELECT DISTINCT upperlevel FROM cathierarchy'
         return self.findCboxItems(SQLquery)
-    
+
     def findSubcats(self):
-        """Find subcategories corresponding to the selected category 
+        """Find subcategories corresponding to the selected category
         and return a list of options for the subcategory combobox.
         """
         cat = self.catvar.get().lstrip()
         SQLquery = 'SELECT lowerlevel FROM cathierarchy WHERE \
                         upperlevel="{}"'.format(cat)
         return self.findCboxItems(SQLquery)
-    
+
     def findCboxItems(self, SQLquery):
         """Return a list of options for a combobox."""
         with sqlite3.connect(self.dbpath) as conn:
@@ -113,17 +113,17 @@ class CatFrm(Frame):
         # the inner padding in a combobox doesn't work, to simmulate the
         # padding on the left side, add a space at the begining of each line
         return tools.leftPadItems(find)
-    
+
     def catHandler(self, event):
-        """Update the subcategory combobox and the scrolled list. 
-        
+        """Update the subcategory combobox and the scrolled list.
+
         Set up the options available in the subcategory combobox.
         Reset the subcategory combobox variable to its default value.
         Update the options in the scrolledlist to all words
         of the selectected category.
         """
-        self.catcb.selection_clear() # remove highlighting from the combobox
-        subcats = self.findSubcats()       
+        self.catcb.selection_clear()  # remove highlighting from the combobox
+        subcats = self.findSubcats()
         self.subcatcb['values'] = subcats
         self.subcatcb.config(state='readonly')
         self.subcatvar.set(' -- Zvolte podkategorii --')
@@ -136,11 +136,11 @@ class CatFrm(Frame):
         if var == self.catvar:
             # looking up the words from a category
             SQLquery = 'SELECT word FROM words WHERE category IN \
-                       (SELECT lowerlevel FROM cathierarchy WHERE upperlevel=?)'
+                      (SELECT lowerlevel FROM cathierarchy WHERE upperlevel=?)'
         else:
             # looking up the words from a subcategory
             SQLquery = 'SELECT word FROM words WHERE category=?'
-        
+
         with sqlite3.connect(self.dbpath) as conn:
             cursor = conn.cursor()
             cursor.execute(SQLquery, (vartext,))
@@ -152,12 +152,10 @@ class CatFrm(Frame):
         """Update the options in the scrolled list to the words
         of the selectected subcategory.
         """
-        self.subcatcb.selection_clear() # remove highlighting from the combobox
+        self.subcatcb.selection_clear()  # remove highlighting from combobox
         wordlist = self.findWords(self.subcatvar)
         self.scrolledlist.setOptions(wordlist)
-        
+
     def mySort(self, alist):
         """Sort a list alphabetically, items starting with a number go last."""
-        return sorted(alist, key = lambda x: (x[0].isdigit(), x.lower()))
-                
-
+        return sorted(alist, key=lambda x: (x[0].isdigit(), x.lower()))
