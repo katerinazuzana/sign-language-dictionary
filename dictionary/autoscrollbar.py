4523ed8de19174d77b7a69020ed599fc64ac8d4f
Katerina <katerina.zuzanakova@gmail.com>
2018-10-29 17:50:56 +0100

    Flake8 check (autoscrollbar.py)

--- dictionary/autoscrollbar.py
+++ dictionary/autoscrollbar.py
@@ -1,17 +1,16 @@
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter import ttk
 
 
-class AutoScrollbar(Scrollbar):
+class AutoScrollbar(ttk.Scrollbar):
     """A scrollbar that hides itself if it's not needed."""
-    
+
     def set(self, low, high):
         if float(low) <= 0 and float(high) >= 1:
             self.grid_forget()
         else:
-            if self.cget('orient') == HORIZONTAL:
-                self.grid(column=0, row=1, sticky=E+W)
+            if self.cget('orient') == tk.HORIZONTAL:
+                self.grid(column=0, row=1, sticky=tk.E+tk.W)
             else:
-                self.grid(column=1, row=0, sticky=N+S)
-        Scrollbar.set(self, low, high)
-
+                self.grid(column=1, row=0, sticky=tk.N+tk.S)
+        ttk.Scrollbar.set(self, low, high)
