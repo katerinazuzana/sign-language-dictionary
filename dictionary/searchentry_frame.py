c05a5f25d1caabb4ee7eb3f1d57863e6584f2302
Katerina <katerina.zuzanakova@gmail.com>
2018-10-30 13:24:18 +0100

    Flake8 check (searchentry_frame.py)

--- dictionary/searchentry_frame.py
+++ dictionary/searchentry_frame.py
@@ -1,16 +1,16 @@
-from tkinter import *
+import tkinter as tk
 import os
 import sqlite3
 from autocomplete_entry import AutocompleteEntry
 import tools
 
 
-class EntFrm(Frame):
+class EntFrm(tk.Frame):
     """A frame with an entry and a search button."""
-    
+
     def __init__(self, parent, dbpath, imgdir, searchfcn, **options):
         """Create an AutocompleteEntry and a Search button.
-        
+
         Arguments:
             parent: a parent tkinter widget
             dbpath (str): the database file path
@@ -29,25 +29,25 @@ class EntFrm(Frame):
         """Create the widgets."""
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        
+
         # create the search button
-        bfrm = Frame(self, height=27, width=50)
+        bfrm = tk.Frame(self, height=27, width=50)
         bfrm.grid_propagate(0)  # size of child doesn't affect size of parent
-        bfrm.grid(column=1, row=0, sticky=N)
+        bfrm.grid(column=1, row=0, sticky=tk.N)
         bfrm.rowconfigure(0, weight=1)
         bfrm.columnconfigure(0, weight=1)
-        
-        self.iconImg = tools.getImage(self.iconPath, 
-                                      width=self.iconSize, 
+
+        self.iconImg = tools.getImage(self.iconPath,
+                                      width=self.iconSize,
                                       height=self.iconSize)
-        Button(bfrm, 
-               image=self.iconImg,
-               command=self.startSearch, 
-               bg=self.bgcolor, 
-               activebackground='ghost white', 
-               borderwidth=0
-               ).grid(column=0, row=0, sticky=N+E+S+W)
-        
+        tk.Button(bfrm,
+                  image=self.iconImg,
+                  command=self.startSearch,
+                  bg=self.bgcolor,
+                  activebackground='ghost white',
+                  borderwidth=0).grid(column=0, row=0,
+                                      sticky=tk.N+tk.E+tk.S+tk.W)
+
         # create a list of all the words contained in the database
         with sqlite3.connect(self.dbpath) as conn:
             cursor = conn.cursor()
@@ -56,30 +56,29 @@ class EntFrm(Frame):
         entries = tools.listOfTuplesToList(allwords)
         # The items of 'entries' will be used in AutocompleteEntry's listbox.
         # In a listbox, there's no option of inner padding, hence
-        # to simmulate the padding on the left side, 
+        # to simmulate the padding on the left side,
         # add a space at the begining of each line.
         entries = tools.leftPadItems(entries)
-        
+
         # create the autocomplete entry
-        self.var = StringVar()
+        self.var = tk.StringVar()
         self.var.set(self.defaultText)
-        self.ent = AutocompleteEntry(self, 
-                                     entries, 
-                                     self.defaultText, 
-                                     self.startSearch, 
-                                     maxEntries=10, 
+        self.ent = AutocompleteEntry(self,
+                                     entries,
+                                     self.defaultText,
+                                     self.startSearch,
+                                     maxEntries=10,
                                      textvariable=self.var)
-        self.ent.grid(column=0, row=0, sticky=N+E+W)
+        self.ent.grid(column=0, row=0, sticky=tk.N+tk.E+tk.W)
         self.ent.config(style="Gray.TEntry")
 
     def startSearch(self, event=None):
         """Hide listbox and do the search if there's some text in the entry."""
         if self.var.get() in ('', self.defaultText):
-            # the user didn't enter any text, prompt them to enter an expression
+            # user didn't enter any text, prompt them to enter an expression
             self.master.showEnterText()
         else:
             self.ent.hideListboxWin()
             self.ent.focus_set()
-            self.ent.icursor(END)
+            self.ent.icursor(tk.END)
             self.searchfcn(self.var.get())
-
